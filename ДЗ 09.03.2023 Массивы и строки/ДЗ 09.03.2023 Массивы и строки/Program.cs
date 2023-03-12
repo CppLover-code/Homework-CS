e34c12bee184af0a02e4039e84d42e80dea412bd
Anna Boiko <annaboiko377@gmail.com>
2023-03-12 16:00:42 +0300

    !

---
+++ ДЗ 09.03.2023 Массивы и строки/ДЗ 09.03.2023 Массивы и строки/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_09._03._2023_Массивы_и_строки
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Задание 1. Работа с одномерным массивом");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(" Задание 2. Работа с двумерным массивом");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(" Задание 3. Шифр Цезаря");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(" Задание 4. Операции над матрицами");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(" Задание 5. Арифметическое выражение");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(" Задание 6. Изменение регистра первой буквы каждого предложения");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(" Задание 7. Проверка текста на недопустимые слова");
+            Console.WriteLine("----------------------------------------");
+
+        }
+    }
+}
---
+++ ДЗ 09.03.2023 Массивы и строки/ДЗ 09.03.2023 Массивы и строки/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ДЗ 09.03.2023 Массивы и строки")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ДЗ 09.03.2023 Массивы и строки")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("06f19163-7122-4bb7-a5f9-edb04461f11f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
