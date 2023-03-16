a200edebad56d2ab34fb9ba11866743c540992ee
Anna Boiko <annaboiko377@gmail.com>
2023-03-16 12:43:03 +0300

    !

---
+++ ДЗ 14.03.2023 Введение в классы/ДЗ 14.03.2023 Введение в/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_14._03._2023_Введение_в
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ДЗ 14.03.2023 Введение в классы/Задание 5/Program5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    internal class Program5
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
