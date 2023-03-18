2eed902820b87e7ee9c0370e5fb94507a44b318c
Anna Boiko <annaboiko377@gmail.com>
2023-03-19 00:54:36 +0300

    !

--- ДЗ 14.03.2023 Введение в классы/ДЗ 14.03.2023 Введение в/Program1.cs
+++ ДЗ 14.03.2023 Введение в классы/ДЗ 14.03.2023 Введение в/Program1.cs
@@ -10,7 +10,38 @@ namespace ДЗ_14._03._2023_Введение_в
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Square\n");
+            Square square = new Square(5, '-');
+            square.ShowSquare();
+        }
+        class Square
+        {
+            public int length;
+            public char symbol;
+            public Square()
+            {
+                Console.WriteLine(" C-tor by default");
+                length = 5;
+                symbol = '*';
+            }
+            public Square(int l, char s) 
+            {
+                Console.WriteLine(" C-tor with 2 param");
+                length = l;
+                symbol = s;
+            }
 
+            public void ShowSquare()
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    for (int k = 0; k < length; k++)
+                    {
+                        Console.Write(symbol + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
--- ДЗ 14.03.2023 Введение в классы/Задание 2/Program2.cs
+++ ДЗ 14.03.2023 Введение в классы/Задание 2/Program2.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,41 @@ namespace Задание_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Palindrome\n");
+
+            long number;
+            while (true)                                               
+            {      
+                Console.WriteLine(" Enter the number: ");
+                if (!long.TryParse(Console.ReadLine(), out number))      // если введенные данные не явл. числом
+                {
+                    Console.WriteLine(" Incorrect input!\n");            // вывод сообщения о неверном вводе
+                }
+                else break;                                              // иначе выход из цикла
+            }
+            string str = number.ToString();                              // преобразование числа в строку
+
+            if (Palindrome.CheckP(str))                                  // если метод вернул true
+            {
+                Console.WriteLine(" This number is a palindrome!");      // ok!
+            }
+            else { Console.WriteLine(" This is NOT a palindrome!"); }    // иначе НЕ ok!
+
+
+        }
+        class Palindrome
+        {         
+            public static string ReverseStr(string str) // метод для реверсирования строки
+            {
+                char[] chars = str.ToCharArray();       // строку в массив char
+                Array.Reverse(chars);                   // реверс массива char
+                return new string(chars);               // возврат строки, содержащей символы массива char
+            }
+            public static bool CheckP(string str)       // метод определения палиндрома
+            {
+                string temp = ReverseStr(str);          // инициализируем строку реверсом
+                return str.Equals(temp);                // возврат результата равенства/неравенства 
+            }
         }
     }
 }
