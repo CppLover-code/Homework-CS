92f22eb6590050a915303861d744274aea85d975
Anna Boiko <annaboiko377@gmail.com>
2023-03-19 01:01:00 +0300

    !

--- ДЗ 14.03.2023 Введение в классы/ДЗ 14.03.2023 Введение в/Program1.cs
+++ ДЗ 14.03.2023 Введение в классы/ДЗ 14.03.2023 Введение в/Program1.cs
@@ -11,33 +11,24 @@ namespace ДЗ_14._03._2023_Введение_в
         static void Main(string[] args)
         {
             Console.WriteLine(" Square\n");
-            Square square = new Square(5, '-');
-            square.ShowSquare();
+
+            Console.WriteLine(" Enter the length of the side of the square:");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Enter character to display square:");
+            char symbol = char.Parse(Console.ReadLine());
+
+            Square.ShowSquare(length,symbol);
         }
         class Square
         {
-            public int length;
-            public char symbol;
-            public Square()
-            {
-                Console.WriteLine(" C-tor by default");
-                length = 5;
-                symbol = '*';
-            }
-            public Square(int l, char s) 
-            {
-                Console.WriteLine(" C-tor with 2 param");
-                length = l;
-                symbol = s;
-            }
-
-            public void ShowSquare()
+            public static void ShowSquare(int length, char s)  // метод для вывода квадрата
             {
                 for (int i = 0; i < length; i++)
                 {
                     for (int k = 0; k < length; k++)
                     {
-                        Console.Write(symbol + " ");
+                        Console.Write(s + " ");
                     }
                     Console.WriteLine();
                 }
