cf14a187b44c16f47abacc8052de3e0d5740e514
Anna Boiko <annaboiko377@gmail.com>
2023-03-19 19:01:34 +0300

    4

--- ДЗ 14.03.2023 Введение в классы/Задание 4/Program4.cs
+++ ДЗ 14.03.2023 Введение в классы/Задание 4/Program4.cs
@@ -10,6 +10,50 @@ namespace Задание_4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Web site ");
+
+            WebSite webSite = new WebSite();
+            webSite.Print();
+        }
+        class WebSite
+        {
+            public string title;        // название сайта
+            public string url;          // путь к сайту
+            public string discription;  // описание сайта
+            public string ip;           // ip адрес сайта
+
+            public WebSite()
+            {
+                Console.WriteLine(" C-tor by default");
+
+                Console.WriteLine("\n Enter site name:");
+                title = Console.ReadLine();
+
+                Console.WriteLine("\n Enter the url:");
+                url = Console.ReadLine();
+
+                Console.WriteLine("\n Enter a description:");
+                discription = Console.ReadLine();
+
+                Console.WriteLine("\n Enter ip address:");
+                ip = Console.ReadLine();
+            }
+
+            public void Print()
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black; 
+                Console.WriteLine("\n -Information about the site-");
+                Console.ResetColor();
+                Console.WriteLine(" Title: \t" + title);
+                Console.WriteLine(" URL: \t\t" + url);
+                Console.WriteLine(" Description: \t" + discription);
+                Console.WriteLine(" IP adress: \t" + ip);
+            }
+            public string GetTitle() { return title; }
+            public string GetUrl() { return url; }
+            public string GetDiscription() {  return discription; }
+            public string GetIp() { return ip; }
         }
     }
 }
