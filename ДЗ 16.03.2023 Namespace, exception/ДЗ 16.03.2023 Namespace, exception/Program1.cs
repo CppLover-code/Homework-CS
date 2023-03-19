fc326b5e0fe1e3e87010720422b759e3f8347e5b
Anna Boiko <annaboiko377@gmail.com>
2023-03-19 23:30:14 +0300

    !

---
+++ ДЗ 16.03.2023 Namespace, exception/ДЗ 16.03.2023 Namespace, exception/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_16._03._2023_Namespace__exception
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 111");
+        }
+    }
+}
