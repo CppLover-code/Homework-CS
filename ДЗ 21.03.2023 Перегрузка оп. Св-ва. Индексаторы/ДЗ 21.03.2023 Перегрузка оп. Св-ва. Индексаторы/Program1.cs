09588e7940f147d292ba9e41b5ee2b352b6ff5af
Anna Boiko <annaboiko377@gmail.com>
2023-03-22 00:29:41 +0300

    !

---
+++ ДЗ 21.03.2023 Перегрузка оп. Св-ва. Индексаторы/ДЗ 21.03.2023 Перегрузка оп. Св-ва. Индексаторы/Program1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ДЗ_21._03._2023_Перегрузка_оп.Св_ва.Индексаторы
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(" Magazine ");
+            Magazine mag = new Magazine();
+            Clear();                    // очистка буфера консоли
+            int choice;
+            do
+            {
+                WriteLine(" Information Menu ");
+                WriteLine(" 1 - get name\n 2 - get year of foundation");
+                WriteLine(" 3 - get description\n 4 - get contact phone");
+                WriteLine(" 5 - get contact mail\n 6 - get all information\n 7 - exit");
+
+                choice = int.Parse(ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        WriteLine($" Magazine name: {mag.Name}\n");
+                        break;
+
+                    case 2:
+                        WriteLine($" Year of foundation: {mag.Year}\n");
+                        break;
+
+                    case 3:
+                        WriteLine($" Description: {mag.Description}\n");
+                        break;
+
+                    case 4:
+                        WriteLine($" Contact phone: {mag.Tel}\n");
+                        break;
+
+                    case 5:
+                        WriteLine($" Contact e-mail: {mag.Mail}\n");
+                        break;
+
+                    case 6:
+                        mag.Print();
+                        break;
+
+                    case 7:
+                        return;
+
+                    default:
+                        WriteLine(" Incorrect input!\n");
+                        break;
+                }
+            } while (true);
+        }
+
+        class Magazine
+        {
+            private string name;   // название журнала
+            private int year;      // год основания
+            private string description;  // описание
+            private long tel;      // контактный телефон
+            private string mail;   // контактный e-mail
+
+            public string Name
+            {
+                get { return name; }
+            }
+            public int Year
+            {
+                get { return year; }
+            }
+            public string Description
+            {
+                get { return description; }
+            }
+            public long Tel
+            {
+                get { return tel; }
+            }
+            public string Mail
+            {
+                get { return mail; }
+            }
+
+
+            public Magazine()
+            {
+                //WriteLine(" C-tor by default");
+
+                WriteLine("\n Enter magazine name:");
+                name = ReadLine();
+
+                WriteLine("\n Enter year of foundation:");
+                year = int.Parse(ReadLine());
+
+                WriteLine("\n Enter a description:");
+                description = ReadLine();
+
+                WriteLine("\n Enter contact phone:");
+                tel = long.Parse(ReadLine());
+
+                WriteLine("\n Enter contact e-mail:");
+                mail = ReadLine();
+            }
+
+            public void Print()
+            {
+                BackgroundColor = ConsoleColor.Blue;
+                ForegroundColor = ConsoleColor.Black;
+                WriteLine("\n -Information about the magazine-");
+                ResetColor();
+                WriteLine(" Name:\t\t\t" + name);
+                WriteLine(" Year of foundation:\t" + year);
+                WriteLine(" Contact phone:\t\t" + tel);
+                WriteLine(" Contact e-mail:\t" + mail + "\n");
+            }
+
+            //public string GetName() { return name; }
+            //public int GetYear() { return year; }
+            //public string GetDescription() { return description; }
+            //public long GetTel() { return tel; }
+            //public string GetMail() { return mail; }
+        }
+        // можно добавить проверку года, телефона
+    }
+}
