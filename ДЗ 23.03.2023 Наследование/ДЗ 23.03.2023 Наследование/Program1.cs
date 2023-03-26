34678c681d9b18d11a0cc3dbc8627893ac884e10
Anna Boiko <annaboiko377@gmail.com>
2023-03-26 17:35:13 +0300

    !

--- ДЗ 23.03.2023 Наследование/ДЗ 23.03.2023 Наследование/Program1.cs
+++ ДЗ 23.03.2023 Наследование/ДЗ 23.03.2023 Наследование/Program1.cs
@@ -5,6 +5,67 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Money money = new Money(25, 30);
+            //money.OutputSum();
+            Money money = new Money();
+            money.Input();
+            money.OutputSum();
+
+        }
+        class Money
+        {
+            public int dollar { get; set; } // целая часть доллара 
+            public int cent { get; set; }   // центы
+
+            public Money()
+            {
+                this.dollar = 0;
+                this.cent = 0;
+            }
+            public Money(int d, int c)
+            {
+                this.dollar = d;
+                this.cent = c;
+            }
+            public void OutputSum()
+            {
+                Console.WriteLine($" Amount of money: {dollar}.{cent} ");
+            }
+            public void Input()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write(" Enter the amount of dollars: ");
+                        dollar = int.Parse(Console.ReadLine()!);
+                        break;
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine(" Incorrect input", ex);
+                    }
+                }
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write(" Enter the amount of cents: ");
+                        cent = int.Parse(Console.ReadLine()!);
+                        break;
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine(" Incorrect input", ex);
+                    }
+                }               
+            }
+            //public void Reduction();
+        }
+        class Product : Money
+        {
+
         }
     }
 }
