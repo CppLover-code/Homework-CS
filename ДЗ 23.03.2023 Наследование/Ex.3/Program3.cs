b5ebaedc2d04cac9e49a18130c146372c49a98b5
Anna Boiko <annaboiko377@gmail.com>
2023-03-28 00:23:57 +0300

    !

--- ДЗ 23.03.2023 Наследование/Ex.3/Program3.cs
+++ ДЗ 23.03.2023 Наследование/Ex.3/Program3.cs
@@ -1,111 +1,121 @@
-﻿namespace Ex._3
+﻿using System.Reflection.Emit;
+using System.Security.Principal;
+
+namespace Ex._3
 {
     internal class Program3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t\t\tMusical instrument");
+            Console.WriteLine("\t\t\t-Musical instrument-");
             Console.Title = "Musical instrument";
+#pragma warning disable CA1416 // Проверка совместимости платформы
+            Console.SetWindowSize(72, 35);
+            
+            MusicalInstrument[] musicalInstrument =
+            {
+                new Violin("Stradivarius"," The Stradivari's sound is described as more \"direct and precise\",\n" +
+                           " responding to the slightest touch with refined direction and elegance."),
+                new Trombone("Trombone-soprano"," The sound color of the trombone is very rich, it can be courageous,\n" +
+                            " bright and formidable, but it can be calm and lyrical, so the\n" +
+                            " instrument was originally used in church works."),
+                new Ukulele("Alt ukulele"," This is one of the varieties of the guitar - a four-stringed plucked\n" +
+                            " musical instrument, which is suitable both for playing chord\n" +
+                            "accompaniment and for performing melodic lines."),  
+                new Сello("Сello 4/4"," The cello belongs to the group of bowed string instruments and has a\n" +
+                          " typical curved shape. It is larger than the viola, has a lower voice,\n" +
+                          " but less double bass and higher in timbre.")
+            };
 
+            foreach (var instrument in musicalInstrument)
+            {
+                int i = ((instrument.GetType().ToString()).IndexOf('+'));
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"\t-{ (instrument.GetType().ToString()).Substring(i + 1)}- ");
+                Console.ResetColor();
+                instrument.Show();
+                instrument.Sound();
+                instrument.Desc();
+                Console.WriteLine();
+            }
         }
-        abstract class MusicalInstrument
+        class MusicalInstrument
         {
             public string? name { get; set; }
             public string? description { get; set; }
-            public abstract void Sound();
-            public abstract void Show();
-            public abstract void Desc();
+            public MusicalInstrument() { }
+            public MusicalInstrument(string n, string d)
+            {
+                name = n;
+                description = d;
+            }
+            public virtual void Sound() { }
+            public virtual void Show() { }
+            public virtual void Desc() { }
         }
         class Violin : MusicalInstrument
         {
-            public Violin()
-            {
-                Console.WriteLine("\n Enter the name of the violin:");
-                name = Console.ReadLine();
-                Console.WriteLine(" Enter a description of the violin:");
-                description = Console.ReadLine();
-            }
+            public Violin (string n, string d) : base(n,d) { }
 
             public override void Sound()
             {
-                Console.WriteLine(" Violin sound: Lya-lya-lya!");
+                Console.WriteLine(" ► Violin sound: Lya-lya-lya!");
             }
             public override void Show()
             {
-                Console.WriteLine(" Violin name: " + name);
+                Console.WriteLine(" ► Violin name:  " + name);
             }
             public override void Desc()
             {
-                Console.WriteLine(" Description of the violin: " + description);
+                Console.WriteLine(" ► Description of the violin:\n" + description);
             }
         }
         class Trombone : MusicalInstrument
         {
-            public Trombone()
-            {
-                Console.WriteLine("\n Enter the name of the trombone:");
-                name = Console.ReadLine();
-                Console.WriteLine(" Enter a description of the trombone:");
-                description = Console.ReadLine();
-            }
-
+            public Trombone (string n, string d) : base(n, d) { }
             public override void Sound()
             {
-                Console.WriteLine(" Tu-tru-tu-tu!");
+                Console.WriteLine(" ► Trombone sound: Tu-tru-tu-tu!");
             }
             public override void Show()
             {
-                Console.WriteLine(" Trombone name " + name);
+                Console.WriteLine(" ► Trombone name:  " + name);
             }
             public override void Desc()
             {
-                Console.WriteLine(" Description of the trombone: " + description);
+                Console.WriteLine(" ► Description of the trombone:\n" + description);
             }
         }
         class Ukulele : MusicalInstrument
         {
-            public Ukulele()
-            {
-                Console.WriteLine("\n Enter the name of the ukulele:");
-                name = Console.ReadLine();
-                Console.WriteLine(" Enter a description of the ukulele:");
-                description = Console.ReadLine();
-            }
-
+            public Ukulele (string n, string d) : base(n, d) { }
             public override void Sound()
             {
-                Console.WriteLine(" Drin'-brin'!");
+                Console.WriteLine(" ► Ukulele sound: Drin'-brin'!");
             }
             public override void Show()
             {
-                Console.WriteLine(" Ukulele name " + name);
+                Console.WriteLine(" ► Ukulele name:  " + name);
             }
             public override void Desc()
             {
-                Console.WriteLine(" Ukulele of the ukulele: " + description);
+                Console.WriteLine(" ► Description of the ukulele:\n" + description);
             }
         }
         class Сello : MusicalInstrument
         {
-            public Сello()
-            {
-                Console.WriteLine("\n Enter the name of the cello:");
-                name = Console.ReadLine();
-                Console.WriteLine(" Enter a description of the cello:");
-                description = Console.ReadLine();
-            }
-
+            public Сello (string n, string d) : base(n, d) { }         
             public override void Sound()
             {
-                Console.WriteLine(" Tru-lya-lya!");
+                Console.WriteLine(" ► Сello sound: Tru-lya-lya!");
             }
             public override void Show()
             {
-                Console.WriteLine(" Сello name " + name);
+                Console.WriteLine(" ► Сello name:  " + name);
             }
             public override void Desc()
             {
-                Console.WriteLine(" Description of the cello: " + description);
+                Console.WriteLine(" ► Description of the cello:\n" + description);
             }
         }
     }
