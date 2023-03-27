1ca8bb82d446af28d940305cac42d852a6629c16
Anna Boiko <annaboiko377@gmail.com>
2023-03-28 01:03:51 +0300

    !

--- ДЗ 24.03.2023 Интерфейсы/Ex.3/Program3.cs
+++ ДЗ 24.03.2023 Интерфейсы/Ex.3/Program3.cs
@@ -2,9 +2,6 @@
 {
     internal class Program3
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        
     }
 }
--- ДЗ 24.03.2023 Интерфейсы/ДЗ 24.03.2023 Интерфейсы/Program1.cs
+++ ДЗ 24.03.2023 Интерфейсы/ДЗ 24.03.2023 Интерфейсы/Program1.cs
@@ -2,9 +2,97 @@
 {
     internal class Program1
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                Array a = new();
+                a.Show();
+                a.Show(" Array with param");
+            }
+        }
+        interface IOutput
+        {
+            void Show();
+            void Show(string info);
+        }
+        interface IOutput2
+        {
+            void ShowEven();
+            void ShowOdd();
+        }
+        interface ICalc
+        {
+            int Less(int valueToCompare);
+            int Greater(int valueToCompare);
+        }
+        interface ICalc2
+        {
+            int CountDistinct();
+            int EqualToValue(int valueToCompare);
+        }
+
+        class Array : IOutput, IOutput2, ICalc, ICalc2
+        {
+            public int[] ar;
+
+            public Array()
+            {
+                ar = new int[5];
+                Random random = new Random();
+                for (int i = 0; i < ar.Length; i++)
+                {
+                    ar[i] = random.Next(1, 10);
+                }
+            }
+            public void Show()
+            {
+                Console.WriteLine(" Array");
+                foreach (int i in ar)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+            public void Show(string info)
+            {
+                Console.WriteLine(info);
+                foreach (int i in ar)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+            ///////////////////////
+            public void ShowEven()
+            {
+
+            }
+            public void ShowOdd()
+            {
+
+            }
+
+            ///////////////////////
+            public int Less(int valueToCompare)
+            {
+                return 1;
+            }
+            public int Greater(int valueToCompare)
+            {  
+                return 1; 
+            }
+
+            ///////////////////////
+            public int CountDistinct()
+            {
+                return 1;
+            }
+            public int EqualToValue(int valueToCompare)
+            {
+                return 1;
+            }
         }
     }
 }
