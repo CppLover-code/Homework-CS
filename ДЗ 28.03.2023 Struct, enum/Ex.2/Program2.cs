30ffc34d74365e8e1411a1ef2483bd522ff2c72f
Anna Boiko <annaboiko377@gmail.com>
2023-03-29 02:58:31 +0300

    !

--- ДЗ 28.03.2023 Struct, enum/Ex.2/Program2.cs
+++ ДЗ 28.03.2023 Struct, enum/Ex.2/Program2.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t\t-Decimal number-");
+            Decimal dec = new Decimal(125);
+            dec.ToBinary();
+            dec.ToOctal();
+            dec.ToHexadecimal();    
+
+        }
+        struct Decimal
+        {
+            int number;
+            public Decimal(int value)
+            {
+                number = value;
+            }
+            public void ToBinary()
+            {
+                string s = Convert.ToString(this.number, 2);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($" {this.number} в двоичной системе счисления: " + s + "\n");
+                Console.ResetColor();
+            }
+            public void ToOctal()
+            {
+                string s = Convert.ToString(this.number, 8);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($" {this.number} в восьмеричной системе счисления: " + s + "\n");
+                Console.ResetColor();
+            }
+            public void ToHexadecimal()
+            {
+                string s = Convert.ToString(this.number, 16);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($" {this.number} в шестнадцатеричной системе счисления: " + s + "\n");
+                Console.ResetColor();
+            }
         }
     }
 }
--- ДЗ 28.03.2023 Struct, enum/Ex.3/Program3.cs
+++ ДЗ 28.03.2023 Struct, enum/Ex.3/Program3.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t\t-RGB color-");
+            RGB rgb = new RGB(35,16,89);
+            rgb.ToHEX();    
+        }
+        struct RGB
+        {
+            int red, green, blue;
+            public RGB(int r,int g, int b)
+            {
+                this.red = r;
+                this.green = g;
+                this.blue = b;
+            }
+            public override string ToString()
+            {
+                return " RGB(" + red + "," + green + "," + blue + ")";
+            }
+            public void ToHEX()
+            {
+                string r = Convert.ToString(this.red, 16);
+                string g = Convert.ToString(this.green, 16);
+                string b = Convert.ToString(this.blue, 16);
+                //Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($" {this} в HEX: #" + r + g + b + "\n");
+                //Console.ResetColor();
+            }
+            public void ToHSL()
+            {
+
+            }
+            public void ToCMYK()
+            {
+
+            }
         }
     }
 }
