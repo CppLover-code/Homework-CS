83c6cf45ce11a47ee2c29df25663d0dd1cec52c7
Anna Boiko <annaboiko377@gmail.com>
2023-03-28 21:29:58 +0300

    !

---
+++ ДЗ 28.03.2023 Struct, enum/ДЗ 28.03.2023 Struct, enum/Program1.cs
@@ -0,0 +1,10 @@
+﻿namespace ДЗ_28._03._2023_Struct__enum
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
