a8854e32e07e481247661512d82eddde308cee71
Anna Boiko <annaboiko377@gmail.com>
2023-03-30 02:25:37 +0300

    !

--- ДЗ 28.03.2023 Struct, enum/Ex.3/Program3.cs
+++ ДЗ 28.03.2023 Struct, enum/Ex.3/Program3.cs
@@ -9,7 +9,6 @@ namespace Ex._3
         {
             Console.Title = " RGB Color Converter";
             Console.WriteLine("\t\t-RGB color converter-");
-            Console.WriteLine($" {Math.Max(5,Math.Max(5,5))}");
             RGB rgb = new RGB();
             Console.ReadLine();
             Console.Clear();
@@ -31,42 +30,47 @@ namespace Ex._3
 
                 switch (key)
                 {
-                    case ConsoleKey.NumPad1:  
+                    case ConsoleKey.NumPad1:
                         rgb.ToHEX();
                         Console.ReadLine();
                         Console.Clear();
                         break;
+
                     case ConsoleKey.NumPad2:
                         rgb.ToHSL();
                         Console.ReadLine();
                         Console.Clear();
                         break;
+
                     case ConsoleKey.NumPad3:
                         rgb.ToCMYK();
                         Console.ReadLine();
                         Console.Clear();
                         break;
+
                     case ConsoleKey.NumPad4:
                         rgb = new RGB();
                         Console.ReadLine();
                         Console.Clear();
                         break;
-                    case ConsoleKey.Escape:                      
+
+                    case ConsoleKey.Escape:
                         return;
+
                     default:
                         Console.WriteLine("\n Incorrect input!\n");
                         Console.ReadLine();
                         Console.Clear();
                         break;
                 }
-            }              
+            }
         }
         struct RGB
         {
             int red, green, blue;
-            public RGB()
+            public RGB()                                              // конструктор без параметров с вводом от пользователя
             {
-                while(true)
+                while (true)
                 {
                     Console.WriteLine("\n Enter color values from 0 to 255 ");
                     try
@@ -78,35 +82,35 @@ namespace Ex._3
                         Console.Write(" Blue: ");
                         blue = int.Parse(Console.ReadLine()!);
 
-                        if (red < 0 || red > 255 ||
-                            green < 0 || green > 255 ||
+                        if (red < 0 || red > 255 ||                   // если введенное число не входит
+                            green < 0 || green > 255 ||               // в диапазон от 0 до 255
                             blue < 0 || blue > 255)
                         {
-                            throw new Exception(" Incorrect input!");
+                            throw new Exception(" Incorrect input!"); // бросаем исключение
                         }
                         break;
                     }
-                    catch (FormatException ex)
-                    {
+                    catch (FormatException ex)                        // ловим некорректный ввод
+                    {                                                 // не удалось спарсить число int
                         Console.WriteLine(" Incorrect input!", ex);
                     }
-                    catch (Exception ex)
+                    catch (Exception ex)                              // ловим нами созданное исключение
                     {
                         Console.WriteLine(ex.Message);
                     }
-                }                      
+                }
             }
-            public RGB(int r,int g, int b)
+            public RGB(int r, int g, int b)                            // конструктор с параметрами
             {
                 this.red = r;
                 this.green = g;
                 this.blue = b;
             }
-            public override string ToString()
+            public override string ToString()                         // перегрузка для вывода структуры
             {
                 return "RGB(" + red + "," + green + "," + blue + ")";
             }
-            public void ToHEX()
+            public void ToHEX()                                       // метод конвертации в HEX
             {
                 string r = Convert.ToString(this.red, 16);
                 string g = Convert.ToString(this.green, 16);
@@ -116,9 +120,9 @@ namespace Ex._3
                 Console.ResetColor();
                 Console.WriteLine($" {this} in HEX: #" + r + g + b + "\n");
             }
-            public void ToHSL()
+            public void ToHSL()                                       // метод конвертации в HSL
             {
-                float r,g,b;
+                float r, g, b;
                 float diff, max, min;
                 double H, S, L;
 
@@ -144,7 +148,7 @@ namespace Ex._3
                     if (max == g) H = Math.Round(60 * (2 + (b - r) / diff));
                     if (max == b) H = Math.Round(60 * (4 + (r - g) / diff));
                 }
-                
+
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\n -Result- \n");
@@ -154,7 +158,7 @@ namespace Ex._3
                     $" Saturation\t{S}%\n" +
                     $" Lightness\t{L}%\n");
             }
-            public void ToCMYK()
+            public void ToCMYK()                                      // метод конвертации в CMYK
             {
                 double cyan, magenta, yellow, black, max;
                 double C, M, Y;
@@ -162,7 +166,7 @@ namespace Ex._3
                 if (red == 0 && green == 0 && blue == 0)
                 {
                     cyan = magenta = yellow = 0;
-                    C = M= Y = 0;
+                    C = M = Y = 0;
                 }
                 else
                 {
@@ -173,10 +177,10 @@ namespace Ex._3
 
                 max = Math.Max(C, Math.Max(M, Y));
 
-                cyan    = Math.Round((max - C) / max * 100);
+                cyan = Math.Round((max - C) / max * 100);
                 magenta = Math.Round((max - M) / max * 100);
-                yellow  = Math.Round((max - Y) / max * 100);
-                black   = Math.Round((1 - max) * 100);
+                yellow = Math.Round((max - Y) / max * 100);
+                black = Math.Round((1 - max) * 100);
 
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\n -Result- \n");
@@ -189,4 +193,5 @@ namespace Ex._3
             }
         }
     }
+    // три метода конвертации основаны на формулах
 }
