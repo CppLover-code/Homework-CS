19afa1affc55301ab7768ba08a1b8c6ee3693eb2
Anna Boiko <annaboiko377@gmail.com>
2023-03-31 01:54:10 +0300

    !

--- ДЗ 30.03.2023 Делегаты, события/Ex.2/Program2.cs
+++ ДЗ 30.03.2023 Делегаты, события/Ex.2/Program2.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-Делегаты Action,Predicate, Func-");
+
+        }
+        static void CurrentTime()
+        {
+
+        }
+        static void CurrentDate()
+        {
+
+        }
+        static void CurrentDayOfWeek()
+        {
+
+        }
+        static void TriangleArea()
+        {
+
+        }
+        static void RectangleArea()
+        {
+
         }
     }
 }
