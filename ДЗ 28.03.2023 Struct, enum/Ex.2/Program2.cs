3848f74461705a7fcbbcc7e596a55503f2c6993d
Anna Boiko <annaboiko377@gmail.com>
2023-03-30 03:18:47 +0300

    !

--- ДЗ 28.03.2023 Struct, enum/Ex.2/Program2.cs
+++ ДЗ 28.03.2023 Struct, enum/Ex.2/Program2.cs
@@ -5,15 +5,83 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t-Decimal number-");
-            Decimal dec = new Decimal(125);
-            dec.ToBinary();
-            dec.ToOctal();
-            dec.ToHexadecimal();    
+            Decimal dec;
+            ConsoleKey key;
 
+            while (true)
+            {
+                Console.WriteLine(" Converter menu");
+                Console.WriteLine(
+                    " 1 - Converting to binary\n" +
+                    " 2 - Converting to octal\n" +
+                    " 3 - Converting to hexadecimal\n" +
+                    " Esc - exit");
+
+                ConsoleKeyInfo info = Console.ReadKey();
+                key = info.Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.NumPad1:
+                        dec = new Decimal();
+                        dec.ToBinary();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad2:
+                        dec = new Decimal();
+                        dec.ToOctal();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad3:
+                        dec = new Decimal();
+                        dec.ToHexadecimal();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.Escape:
+                        return;
+
+                    default:
+                        Console.WriteLine("\n Incorrect input!\n");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
         }
         struct Decimal
         {
             int number;
+            public Decimal()                                          // конструктор без параметров с вводом от пользователя
+            {
+                while (true)
+                {
+                    Console.WriteLine("\n Enter a number:");
+                    try
+                    {
+                        number = int.Parse(Console.ReadLine()!);                      
+
+                        if (number < 0)
+                        {
+                            throw new Exception(" Incorrect input!"); // бросаем исключение
+                        }
+                        break;
+                    }
+                    catch (FormatException ex)                        // ловим некорректный ввод
+                    {                                                 
+                        Console.WriteLine(" Incorrect input!", ex);
+                    }
+                    catch (Exception ex)                              // ловим нами созданное исключение
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
             public Decimal(int value)
             {
                 number = value;
@@ -22,21 +90,21 @@
             {
                 string s = Convert.ToString(this.number, 2);
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($" {this.number} в двоичной системе счисления: " + s + "\n");
+                Console.WriteLine($" Result: {this.number} in binary \u2192 " + s + "\n");
                 Console.ResetColor();
             }
             public void ToOctal()
             {
                 string s = Convert.ToString(this.number, 8);
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($" {this.number} в восьмеричной системе счисления: " + s + "\n");
+                Console.WriteLine($" Result: {this.number} in octal \u2192 " + s + "\n");
                 Console.ResetColor();
             }
             public void ToHexadecimal()
             {
                 string s = Convert.ToString(this.number, 16);
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($" {this.number} в шестнадцатеричной системе счисления: " + s + "\n");
+                Console.WriteLine($" Result: {this.number} in hexadecimal \u2192 " + s + "\n");
                 Console.ResetColor();
             }
         }
--- ДЗ 28.03.2023 Struct, enum/ДЗ 28.03.2023 Struct, enum/Program1.cs
+++ ДЗ 28.03.2023 Struct, enum/ДЗ 28.03.2023 Struct, enum/Program1.cs
@@ -1,36 +1,104 @@
-﻿namespace ДЗ_28._03._2023_Struct__enum
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Numerics;
+
+namespace ДЗ_28._03._2023_Struct__enum
 {
     internal class Program1
     {
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t-3d vector-\n");
-            Vector vector = new Vector(5,6,7);
-            Console.WriteLine(vector);
+            Vector vect;
 
-            vector.Mult(2);
-            Console.WriteLine(vector);
+            ConsoleKey key;
 
-            Vector vector1 = new Vector(6,7,8);
-            Vector vector2;// = new Vector(); //vector2 = vector2.Add(vector,vector1);
-            vector2 = vector + vector1;
-            Console.WriteLine(vector);
-            Console.WriteLine(vector1);
-            Console.WriteLine(vector2);
-            Console.WriteLine();
-            Vector vector3;
-            vector3 = vector1 - vector;
-            Console.WriteLine(vector);
-            Console.WriteLine(vector1);
-            Console.WriteLine(vector3);
-            Console.WriteLine();
+            while (true)
+            {
+                Console.WriteLine(" Vector menu");
+                Console.WriteLine(
+                    " 1 - Multiply by a number\n" +
+                    " 2 - Vector addition\n" +
+                    " 3 - Vector subtraction\n" +
+                    " Esc - exit");
 
+                ConsoleKeyInfo info = Console.ReadKey();
+                key = info.Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.NumPad1:
+                        vect = new();
+                        vect.Mult();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("\n -Result-\n" + vect);
+                        Console.ResetColor();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad2:
+                        Console.WriteLine("\n First vector ");
+                        Vector a = new Vector();
+                        Console.WriteLine(" Second vector ");
+                        Vector b = new Vector();
+                        vect = a + b;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("\n -Result-\n" + vect);
+                        Console.ResetColor();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad3:
+                        Console.WriteLine("\n First vector ");
+                        Vector c = new Vector();
+                        Console.WriteLine("\n Second vector ");
+                        Vector d = new Vector();
+                        vect = c - d;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("\n -Result-\n" + vect);
+                        Console.ResetColor();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.Escape:
+                        return;
+
+                    default:
+                        Console.WriteLine("\n Incorrect input!\n");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
         }
         struct Vector
         {
             int x;
             int y;
             int z;
+            public Vector()                                       // конструктор без параметров с вводом от пользователя
+            {
+                while (true)
+                {
+                    Console.WriteLine("\n Enter vector data ");
+                    try
+                    {
+                        Console.Write(" X: ");
+                        x = int.Parse(Console.ReadLine()!);
+                        Console.Write(" Y: ");
+                        y = int.Parse(Console.ReadLine()!);
+                        Console.Write(" Z: ");
+                        z = int.Parse(Console.ReadLine()!);
+                        break;
+                    }
+                    catch (FormatException ex)                        // ловим некорректный ввод
+                    {                                                 // не удалось спарсить число int
+                        Console.WriteLine(" Incorrect input!", ex);
+                    }
+                }                      
+            }
             public Vector(int x, int y, int z)
             {
                 this.x = x;
@@ -41,14 +109,27 @@
             {
                 return " 3-D Vector:" + "\n coord X - " + x + "\n coord Y - " + y + "\n coord Z - " + z;
             }
-            public void Mult(int numb)
+            public void Mult()
             {
+                int numb;
+                while (true)
+                {
+                    Console.WriteLine(" Enter a number to multiply:");
+                    try { numb = int.Parse(Console.ReadLine()!); break; }
+                    catch (FormatException ex) { Console.WriteLine(" Incorrect input!", ex); }                 
+                }
+                
                 this.x *= numb;
                 this.y *= numb;
                 this.z *= numb;
             }
-            public Vector Add(Vector a, Vector b)
+            public Vector Add()
             {
+                Console.WriteLine(" Enter data about the first vector ");
+                Vector a = new Vector();
+                Console.WriteLine(" Enter data about the second vector ");
+                Vector b = new Vector();
+
                 Vector temp = new Vector();
                 temp.x = a.x + b.x;
                 temp.y = a.y + b.y;
