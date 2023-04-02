84532de29a33c53c801cfebd4784d8ea92f41b1b
Anna Boiko <annaboiko377@gmail.com>
2023-04-03 01:04:07 +0300

    !

--- ДЗ 30.03.2023 Делегаты, события/Ex.3/Program3.cs
+++ ДЗ 30.03.2023 Делегаты, события/Ex.3/Program3.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
+using System.Numerics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ex._3
 {
@@ -8,49 +10,105 @@ namespace Ex._3
     {
         static void Main(string[] args)
         {
-            CreditCard card = new (222111555, "Oliver Hendels", "21.02.2026", new Account(100,500, 1414));
+            Console.Title = " Credit Card";
+            Console.WriteLine(" -Enter your credit card details-");
+            CreditCard card = new();
+            Thread.Sleep(1000);
+            Console.Clear();
+            //CreditCard card = new(222111555, "Julia Roberts", "21.02.2028", new Account(100, 500, 1414));
             //user1.account.Notify += DisplayMessage; // ответ банка Добавляем обработчик для события Notify          
             //user1.AccountAction += DisplayUserMessage; // действие пользователя Добавляем обработчик для события Notify
             card.account.MoneyNotify += (object? sender, AccoutEventArgs e) =>
             {
-                Console.WriteLine(" Ответ банка:\n " + e.Message + e.Sum + ".\n Баланс средств: " + e.Balance);
-                Console.WriteLine("---------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(" Bank message:\n " + e.Message + e.Sum + " UAH" +
+                    ".\n Current balance: " + e.Balance + " UAH");
                 Console.ResetColor();
             };           
             card.MoneyAction += delegate (object? sender, AccoutEventArgs e)
             {
                 if (sender is CreditCard card)
                 {
-                    Console.WriteLine(e.Message + e.Sum + " клиентом: " + card.holderName);
+                    Console.WriteLine(e.Message + e.Sum + " UAH" + " by the client: " + card.holderName);
                 }
             };
             card.account.PinNotify += (object? sender, AccoutEventArgs2 e) =>
             {
-                Console.WriteLine(" Ответ банка:\n " + e.Message);
-                Console.WriteLine("---------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(" Bank message:\n " + e.Message);
                 Console.ResetColor();
             };
             card.PinAction += delegate (object? sender, AccoutEventArgs2 e)
             {
                 if (sender is CreditCard card)
                 {
-                    Console.WriteLine(e.Message + " клиентом: " + card.holderName);
+                    Console.WriteLine(e.Message + " by the client: " + card.holderName);
                 }
             };
-            card.account.GoalNotify += (object? sender, AccoutEventArgs2 e) =>
+            card.account.AchievementNotify += (object? sender, AccoutEventArgs2 e) =>
             {
-                Console.WriteLine(" Ответ банка:\n " + e.Message);
-                Console.WriteLine("---------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(" Bank message:\n " + e.Message + " UAH");
                 Console.ResetColor();
             };
             
+            
+            ConsoleKey key;
 
-            card.Put(100);
-            card.Take(120);
-            card.Take(100);
-            //card.Put(100);
-            card.Take(500);
-            card.ChangePin();
+            while (true)
+            {
+                Console.WriteLine("-Credit Card Menu-\n\n" +
+                    " 1 - Put money\n" +
+                    " 2 - Withdraw money\n" +
+                    " 3 - Change pin code\n" +
+                    " 4 - Card info\n" +
+                    " Esc - exit\n");
+                Console.Write(" Make a choice on NumPad: ");
+
+                ConsoleKeyInfo info = Console.ReadKey();
+                Console.WriteLine();
+
+                key = info.Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.NumPad1:
+                        float put = CheckAmount("to top up card");
+                        card.Put(put);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad2:
+                        float take = CheckAmount("to withdraw money");                                               
+                        card.Take(take);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad3:
+                        card.ChangePin();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.NumPad4:
+                        card.Info();
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    case ConsoleKey.Escape:
+                        Console.WriteLine(" Bye! Bye!");
+                        return;
+
+                    default:
+                        Console.WriteLine("\n Incorrect input!\n");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
         }
 
         
@@ -80,12 +138,12 @@ namespace Ex._3
             public EventHandler<AccoutEventArgs>? MoneyNotify; // 1. Define an event// унифицированный обработчик событий
                                                           // ДЕЛЕГАТ для события
             public EventHandler<AccoutEventArgs2>? PinNotify;
-            public EventHandler<AccoutEventArgs2>? GoalNotify;
+            public EventHandler<AccoutEventArgs2>? AchievementNotify;
 
             public float Balance { get; set; } = 0; // текущий баланс
-            public float Limit { get; set; } // кредитный лимит
-            public float Debt { get; set; }       // долг по кредиту
-            public float CopyL;              // копия кредитного лимита
+            public float Limit { get; set; }        // кредитный лимит
+            public float Debt { get; set; }         // долг по кредиту
+            public float CopyL;                     // копия кредитного лимита
             public int pinCode { get; set; }
             public Account(float sum, float cl,int pin)
             { 
@@ -94,7 +152,14 @@ namespace Ex._3
                 this.Debt = 0;
                 this.CopyL = this.Limit;
                 this.pinCode = pin;
-
+            }
+            public Account()
+            {
+                this.Balance = CheckAmount("of the account balance");
+                this.Limit = CheckAmount("of the credit limit");
+                this.Debt = 0;
+                this.CopyL = this.Limit;
+                this.pinCode = CheckPin("");
             }
             public void Put(float top_up)
             {
@@ -115,11 +180,13 @@ namespace Ex._3
                     Debt = CopyL - Limit;
                 }
 
-                MoneyNotify?.Invoke(this, new AccoutEventArgs("Положено на счет ", top_up, Balance)); // 2. Raise Event
+                MoneyNotify!.Invoke(this, new AccoutEventArgs("Account topped up with ", top_up, Balance)); // 2. Raise Event
 
-                if(Balance >= 100)
+                int achievement = 10000;
+
+                if (Balance >= achievement)
                 {
-                    GoalNotify?.Invoke(this, new AccoutEventArgs2(" Ура! Ваш баланс больше 100 грн"));
+                    AchievementNotify?.Invoke(this, new AccoutEventArgs2($"Perfect! Your balance is more than {achievement}"));
                 }
             }
             public void Take(float sum)
@@ -127,7 +194,7 @@ namespace Ex._3
                 if (sum <= Balance)                         // если сумма снятия меньше или равна текущему балансу  
                 {                                           // уменьшаем баланс на сумму затраты  
                     Balance -= sum;
-                    MoneyNotify?.Invoke(this, new AccoutEventArgs("Снято со счета ", sum, Balance)); // 2. Raise Event
+                    MoneyNotify?.Invoke(this, new AccoutEventArgs("Withdrawn from account ", sum, Balance)); // 2. Raise Event
                 }
                 else if ((sum > Balance) &&                 // если сумма снятия больше текущего баланса И
                     (sum <= Balance + Limit))               // сумма снятия меньше либо равна сумме баланса с кред. лимитом
@@ -135,23 +202,22 @@ namespace Ex._3
                     Limit = Balance + Limit - sum;          // кред. лимит уменьшается
                     Balance = 0;                            // баланс текущий обнуляется
                     Debt = CopyL - Limit;                   // задолженность вычисляется
-                    MoneyNotify?.Invoke(this, new AccoutEventArgs($"Снято со счета {sum} " +
-                        $"Current credit funds {CopyL - Debt}" +
-                        $" Current credit card debt ", Debt, Balance)); // 2. Raise Event
+                    MoneyNotify?.Invoke(this, new AccoutEventArgs($"Withdrawn from account {sum} UAH\n" +
+                        $" Current credit funds: {CopyL - Debt} UAH\n" +
+                        $" Current credit card debt: ", Debt, Balance)); // 2. Raise Event
                 }
                 else if (sum > Balance + Limit)             // если сумма снятия больше суммы баланса с лимитом,
                 {                                           // выводим сообщение
                     Console.ForegroundColor = ConsoleColor.Red;
-                    MoneyNotify?.Invoke(this, new AccoutEventArgs("Недостаточно денег для снятия!\n" +
-                        $" Current credit funds {CopyL - Debt}\n" +
-                        $" Current credit card debt ", Debt, Balance)); // 2. Raise Event
+                    MoneyNotify?.Invoke(this, new AccoutEventArgs("Not enough money to withdraw!\n" +
+                        $" Current credit funds: {CopyL - Debt} UAH\n" +
+                        $" Current credit card debt: ", Debt, Balance)); // 2. Raise Event
                 }               
             }
             public void ChangePin()
             {
-                Console.WriteLine(" Введите новый пин-код:");
-                pinCode = int.Parse(Console.ReadLine()!);
-                PinNotify?.Invoke(this, new AccoutEventArgs2(" Пин-код успешно изменен")); // 2. Raise Event
+                pinCode = CheckPin("new");
+                PinNotify?.Invoke(this, new AccoutEventArgs2(" PIN code has been successfully changed!")); // 2. Raise Event
             }
         }
         public class CreditCard // издатель события
@@ -170,22 +236,132 @@ namespace Ex._3
                 this.expDate = ed;
                 this.account = account;
             }
+            public CreditCard()
+            {
+                this.cardNumber = CheckNumber();
+                Console.Write("\n Enter сardholder name: ");
+                this.holderName = Console.ReadLine();
+                this.expDate = CheckDate();
+                this.account = new Account();
+            }
             public void Take(float sum)
             {                                   // новое событие вызов
-                MoneyAction?.Invoke(this, new AccoutEventArgs("Попытка снятия со счета суммы в размере ", sum, account.Balance));
+                MoneyAction?.Invoke(this, new AccoutEventArgs(" Попытка снятия со счета суммы в размере ", sum, account.Balance));
                 account?.Take(sum);
             }
             public void Put(float sum)
             {
-                MoneyAction?.Invoke(this, new AccoutEventArgs("Попытка положить на счет суммы в размере ", sum, account.Balance));
+                MoneyAction?.Invoke(this, new AccoutEventArgs(" Попытка положить на счет суммы в размере ", sum, account.Balance));
                 account?.Put(sum);
             }
             public void ChangePin()
             {
-                PinAction?.Invoke(this, new AccoutEventArgs2("Попытка смены пин-кода "));
+                PinAction?.Invoke(this, new AccoutEventArgs2(" Попытка смены пин-кода "));
                 account?.ChangePin();
             }
+            public void Info()
+            {
+                Console.WriteLine("\n\t-Credit card-");
+                Console.WriteLine($" Owner: \t{holderName}");
+                Console.WriteLine($" Number: \t{cardNumber}");
+                Console.WriteLine($" Exp. d.: \t{expDate}");
+                Console.WriteLine($" Balance: \t{account?.Balance} UAH");
+                Console.WriteLine($" Credit funds: \t{account?.Limit} UAH");
+                Console.WriteLine($" Credit debt: \t{account?.Debt} UAH");
+            }
+        }
+        static long CheckNumber()                    // проверка номера счета
+        {
+            long numb;
 
+            while (true)
+            {
+                Console.Write("\n Enter card number: ");
+                try
+                {
+                    numb = long.Parse(Console.ReadLine()!);
+
+                    long temp = numb;
+                    int i = 0;
+
+                    for (; temp > 0; i++) { temp /= 10; } // считаем кол-во цифр в числе
+
+                    if (numb < 0 || i != 16)
+                    {
+                        throw new Exception(" Incorrect number!");
+                    }
+                    break;
+                }
+                catch (FormatException ex) { Console.WriteLine(" Incorrect number!", ex); }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+            
+            return numb;
+        }
+        static float CheckAmount(string s)           // проверка вводимых сумм
+        {
+            float sum;
+            while (true)
+            {
+                Console.Write($"\n Enter the amount {s}: ");
+                try
+                {
+                    sum = float.Parse(Console.ReadLine()!);
+                    if (sum < 0)               
+                    {
+                        throw new Exception(" Incorrect amount!");
+                    }
+                    break;
+                }
+                catch (FormatException ex) { Console.WriteLine(" Incorrect amount!", ex); }
+            }
+            return sum;
+        }
+        static string CheckDate()                    // проверка даты 
+        {
+            DateTime date;
+            string str;
+            while (true)
+            {
+                Console.Write("\n Enter the date (dd.mm.yyyy): ");
+                try
+                {
+                    date = DateTime.Parse(Console.ReadLine()!);                  
+                    break;
+                }
+                catch (FormatException ex) { Console.WriteLine(" Incorrect amount!", ex); }
+            }
+
+            string[] subs = date.ToString().Split(" ");
+            str = subs[0];
+
+            return str;
+        }
+        static int CheckPin(string s)                // проверка пин кода
+        {
+            int pin;
+            while (true)
+            {
+                Console.Write($"\n Enter {s} pin code (four digits): ");
+                try
+                {
+                    pin = int.Parse(Console.ReadLine()!);
+
+                    int temp = pin;
+                    int i = 0;
+
+                    for (; temp > 0; i++) { temp /= 10; } // считаем кол-во цифр в числе
+
+                    if (pin < 0 || i != 4)
+                    {
+                        throw new Exception(" Incorrect pin!");
+                    }
+                    break;
+                }
+                catch (FormatException ex) { Console.WriteLine(" Incorrect pin!", ex); }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+            return pin;
         }
     }
 }
