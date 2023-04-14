c3b6e4af13123833711cd27b258d14d94cbb52fe
Anna Boiko <annaboiko377@gmail.com>
2023-04-15 00:21:51 +0300

    !

--- ДЗ 31.03.2023 События, записи/Ex.6/Program6.cs
+++ ДЗ 31.03.2023 События, записи/Ex.6/Program6.cs
@@ -1,32 +1,60 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 namespace Ex._6
 {
 
     internal class Program6
     {
-        delegate string Find(string txt, StringBuilder w );
-        static Find wrd = (txt, w) =>
-        {
-            if (txt.Contains(w.ToString()))
+        delegate string Find(string txt, string w);
+        static Find wrd = (txt, w) =>                          // лямбда выражение
+        {           
+            if (txt.ToUpper().Contains(w.Trim(' ').ToUpper())) // убираем ненужные случайно введенные пробелы в слове и проверяем 
+            {                                                  // содержит ли текст слово независимо от регистра
+                Console.ForegroundColor = ConsoleColor.Green;
                 return "the word was found!";
+            }                
             else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
                 return "word was not found!";
+            }             
         };
-
-        // поиск слова должен учитывать пробел с двух сторон, иначе ищет одну букву в целом слове
 
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t-Using a lambda expression to find a word in text-");
             Console.WriteLine(" Enter text:");
-            // сделать проверку введенного текста
-            string text = Console.ReadLine()!;
-            Console.WriteLine(" Enter a search word: ");
-            // сделать проверку введенного слова
-            StringBuilder word = new (Console.ReadLine()!); // f может и не нужен тут стринг билдер
+            string text = CheckStr();
 
-            word[1] = word[0];
+            Console.WriteLine(" Enter a search word: ");
+            string word = CheckStr();
+
             Console.WriteLine(" Result: " + wrd(text, word));
+            Console.ResetColor();   
+        }
+        static string CheckStr() // метод для проверки введенных данных
+        {
+            string str; 
+               
+            while (true)
+            {
+                try
+                {
+                    str = Console.ReadLine()!;              
+                    if(String.IsNullOrWhiteSpace(str))              // если введенные данные являются пустыми или пробелом,
+                    {
+                        throw new Exception(" Incorrect input!");   // то бросаем исключение
+                    }
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+            }                
+            return str;                                            // возврат введенных данных после проверки
         }
     }
 }
