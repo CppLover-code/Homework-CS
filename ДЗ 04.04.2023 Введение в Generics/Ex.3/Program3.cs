ec482da0bf39fd1471cd1fc2dc343fdd2d626214
Anna Boiko <annaboiko377@gmail.com>
2023-04-25 02:18:08 +0300

    !

--- ДЗ 04.04.2023 Введение в Generics/Ex.3/Program3.cs
+++ ДЗ 04.04.2023 Введение в Generics/Ex.3/Program3.cs
@@ -4,31 +4,68 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            var person = new List<Person>() 
+            { 
+                new Person("Тарас Шевченко"),
+                new Person("Леся Украинка"),
+                new Person("Иван Франко"),
+                new Person("Ольга Кобылянская"),
+                new Person("Сергей Жадан"),
+                new Person("Иван Котляревский"),
+                new Person("Оксана Забужко"),
+                new Person("Николай Зеров"),
+                new Person("Борис Олейник"),
+                new Person("Пантелеймон Кулиш"),
+                new Person("Иван Малкович")
+            };
+            Cafe cafe = new Cafe("Берёзка", 5, person);
+            cafe.MainInfo();
+            var openCafe = new OpenCafe();
 
-        
-        interface IFree
-        {
-            int Free();
+            while(cafe.QueLine?.persons.Count != 0)
+            {
+                openCafe.Open(cafe);
+            }           
         }
-        class Cafe : IFree
+   
+        class Cafe
         {
             public string? Title { get; set; } // название кафе
             public int Table { get; set; }     // кол-во столиков
-            Line? QueLine { get; set; }        // очередь
-            int IFree.Free()                   // кол-во свободных столиков
+            public Line? QueLine { get; set; }        // очередь
+            public Cafe(string title, int table, List<Person> people)
+            {
+                Title = title;
+                Table = table;
+                QueLine = new Line(people);
+            }          
+            public void MainInfo()
+            {
+                Console.WriteLine($" Кафе \"{Title}\"\n Всего столиков - {Table}");
+            }
+            public int FreeTable()
             {
                 Random rnd = new Random();
-                int value = rnd.Next(0, 5);
+                int value = rnd.Next(1, 4);
+                Console.WriteLine($"\n Свободных столиков - {value}");
                 return value;
             }
 
 
         }
+        class OpenCafe
+        {
+            public void Open(Cafe cafe)
+            {
+                int free = cafe.FreeTable();
+                cafe.QueLine?.ShowLine();
+                cafe.QueLine?.RemovePerson(free);
+            }
+        }
+        
         class Line
         {
-            Queue<Person> persons = new();
+            public Queue<Person> persons = new();
             public Line() { }       // конструктор по умолчанию
             public Line(string p)   // конструктор с параметрами
             {
@@ -54,10 +91,34 @@
                 for (int i = 0; i < count; i++)
                      persons.Enqueue(new Person());
             }
-            public void Remove(int count) // удаление гостей из очереди
+            public void Remove(int count) // удаление гостей из очереди без брони
             {
                 for (int i = 0; i < count; i++)
                     persons.Dequeue();
+            }
+            public void RemovePerson(int value)
+            {
+                var temp = persons.ToList();
+
+                    foreach (var guest in persons)
+                    {
+                        if(guest.Reserved())
+                        {
+                            Console.WriteLine($" {guest.Name} проходит по брони!");
+                            temp.Remove(guest);
+                            value--;
+                            //persons.Clear();
+                            persons = new Queue<Person>(temp);
+                        }
+                    }
+                Console.WriteLine($" Value {value}");
+                
+                while(value > 0 && persons.Count > 0)
+                {
+                    Console.WriteLine($" {persons.Peek().Name} проходит из живой очереди!");
+                    persons.Dequeue();
+                    value--;
+                }
             }
         }
         interface IReserved
@@ -76,13 +137,13 @@
             {
                 this.Name = name;
             }
-            bool IReserved.Reserved()         // есть ли бронь у человека
+            public bool Reserved()         // есть ли бронь у человека
             {
                 Random rnd = new Random();
-                int value = rnd.Next(0, 10);
+                int value = rnd.Next(0, 20);
                 if (value == 5) return true;
                 else return false;
-            }
+            }          
         }
     }
 }
