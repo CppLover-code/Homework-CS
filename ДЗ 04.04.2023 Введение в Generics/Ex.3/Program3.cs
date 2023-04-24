e9d137b7cc7e106ec2cbccb6938d155aa1180498
Anna Boiko <annaboiko377@gmail.com>
2023-04-24 04:30:19 +0300

    !

--- ДЗ 04.04.2023 Введение в Generics/Ex.3/Program3.cs
+++ ДЗ 04.04.2023 Введение в Generics/Ex.3/Program3.cs
@@ -6,5 +6,75 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        
+        interface IFree
+        {
+            int Free();
+        }
+        class Cafe : IFree
+        {
+            public string? Title { get; set; } // название кафе
+            public int Table { get; set; }     // кол-во столиков
+            int IFree.Free()                   // кол-во свободных столиков
+            {
+                Random rnd = new Random();
+                int value = rnd.Next(0, 5);
+                return value;
+            }
+        }
+        class Line
+        {
+            Queue<Person> persons = new();
+            public Line() { }       // конструктор по умолчанию
+            public Line(string p)   // конструктор с параметрами
+            {
+                this.persons.Enqueue(new Person() { Name = "Tom" });
+            }
+            public void ShowLine()  // показ очереди
+            {
+                Console.WriteLine("Сейчас в очереди {0} человек", persons.Count);
+                int numb = 1;
+                foreach (Person p in persons)
+                {
+                    Console.WriteLine(numb + ". " + p.Name);
+                    numb++;
+                }
+            }
+            public void Add(int count)    // добавление гостей в очередь
+            {
+                for (int i = 0; i < count; i++)
+                     persons.Enqueue(new Person());
+            }
+            public void Remove(int count) // удаление гостей из очереди
+            {
+                for (int i = 0; i < count; i++)
+                    persons.Dequeue();
+            }
+        }
+        interface IReserved
+        {
+            bool Reserved();
+        }
+        class Person : IReserved
+        {
+            public string? Name { get; set; } // имя
+            public Person()
+            {
+                Console.WriteLine(" Введите фамилию и имя гостя:");
+                Name = Console.ReadLine();
+            }
+            public Person(string name)
+            {
+                this.Name = name;
+            }
+            bool IReserved.Reserved()         // есть ли бронь у человека
+            {
+                Random rnd = new Random();
+                int value = rnd.Next(0, 10);
+                if (value == 5) return true;
+                else return false;
+            }
+        }
     }
 }
