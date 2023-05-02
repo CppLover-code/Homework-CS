a998a2bf2c967d1fbb9aed7bbe4f118679766f37
Anna Boiko <annaboiko377@gmail.com>
2023-05-03 01:19:28 +0300

    !

--- ДЗ 07.04.2023 Вз-ие с файловой системой/ДЗ 07.04.2023 Вз-ие с файловой системой/Program1.cs
+++ ДЗ 07.04.2023 Вз-ие с файловой системой/ДЗ 07.04.2023 Вз-ие с файловой системой/Program1.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t-Collection of poetries-");
+            Console.Title = "Сборник стихов";
             List<Poetry> poetries = new List<Poetry>()
             {
                 new Poetry("Бычок", "Агния Барто", 1927,
@@ -18,13 +18,61 @@
             };
 
             Collection collection = new Collection(poetries);
-            collection.ShowCollection();
-            while(true)
+            //collection.ShowCollection();
+            int choice;
+            
+            while (true)
             {
                 Console.WriteLine("\tМеню\n" +
                     " 1 - показать все стихи\n" +
-                    " 2 - ");
+                    " 2 - добавить стих\n" +
+                    " 3 - удалить стих\n" +
+                    " 4 - изменить информацию о стихе\n" +
+                    " 5 - сохранить сборник стихов в файл\n" +
+                    " 6 - загрузить сборник стихов из файла\n" +
+                    " 0 - выход\n");
+                Console.Write(" Сделайте выбор: ");                   
+                try
+                {
+                    choice = int.Parse(Console.ReadLine()!);
+                    if(choice < 0 || choice > 6)
+                    {
+                        throw new Exception(" Некорректный выбор!");
+                    }
+                    break;
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine(" Некорректный ввод!");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
 
+            switch (choice)
+            {
+                //case 0:
+
+                case 1:
+                    collection.ShowCollection();
+                    break;
+                case 2:
+                    collection.Add();
+                    break;
+                case 3:
+                    collection.Remove();
+                    break;
+                case 4:
+                    collection.Change();
+                    break;
+                case 5:
+                    collection.ShowCollection();
+                    break;
+                case 6:
+                    collection.ShowCollection();
+                    break;
             }
         }
         class Poetry
@@ -76,21 +124,152 @@
             }
             public void ShowCollection()
             {
+                int id = 1;
                 foreach (var poetry in poetries)
+                {
+                    Console.WriteLine($" Стих #{id}");
                     poetry.ShowPoetry();
+                    id++;
+                }    
+            }
+            public void ShowMainInfoCollection() // показ названия и автора стиха
+            {
+                int id = 1;
+                foreach (var poetry in poetries)
+                {
+                    Console.WriteLine($" {id}. \"{poetry.Title}\", {poetry.Author}");
+                    id++;
+                }
             }
             public void Add()   // добавление стиха
             {
                 poetries.Add(new Poetry());
             }
-            public void Remove(int ind) // удаление по индексу
+            public void Remove() // удаление по индексу
             {
+                if(poetries.Count == 0)
+                {
+                    Console.WriteLine(" Сборник стихов пуст!");
+                    return;
+                }
+                ShowMainInfoCollection();
+                int ind;
+                while(true)
+                {
+                    Console.WriteLine(" Введите номер стиха для удаления: ");
+                    try
+                    {
+                        ind = int.Parse(Console.ReadLine()!);
+                        if(ind < 0 || ind > poetries.Count)
+                        {
+                            throw new Exception(" Некорректный выбор!");
+                        }
+                        break;
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine(" Некорректный ввод!");
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                
                 poetries.RemoveAt(ind);
             }
             public void Change()
             {
+                if (poetries.Count == 0)
+                {
+                    Console.WriteLine(" Сборник стихов пуст!");
+                    return;
+                }
+                ShowMainInfoCollection();
+                int ind; 
+                while (true)
+                {
+                    Console.WriteLine(" Введите номер стиха для изменения: ");
+                    try
+                    {
+                        ind = int.Parse(Console.ReadLine()!);
+                        if (ind < 0 || ind > poetries.Count)
+                        {
+                            throw new Exception(" Некорректный выбор!");
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine(" Некорректный ввод!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
 
+                int ch;
+                while (true)
+                {
+                    Console.WriteLine("\tПункты для изменения\n" +
+                    " 1 - название стиха\n" +
+                    " 2 - ФИО автора\n" +
+                    " 3 - год написания\n" +
+                    " 4 - текст стиха\n" +
+                    " 5 - тема стиха\n");
+                    Console.Write(" Сделайте выбор: ");
+                    try
+                    {
+                        ch = int.Parse(Console.ReadLine()!);
+                        if (ch < 1 || ch > 5)
+                        {
+                            throw new Exception(" Некорректный выбор!");
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine(" Некорректный ввод!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                string newInfo;
+                int newDate;
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine(" Введите обновленное название:");
+                        newInfo = Console.ReadLine()!;
+                        poetries[ind].Title = newInfo;
+                        break;
+                    case 2:
+                        Console.WriteLine(" Введите обновлённую информацию об авторе (ФИО):");
+                        newInfo = Console.ReadLine()!;
+                        poetries[ind].Author = newInfo;
+                        break;
+                    case 3:
+                        Console.WriteLine(" Введите обновлённый год написания:");
+                        newDate = int.Parse(Console.ReadLine()!);
+                        poetries[ind].Date = newDate;
+                        break;
+                    case 4:
+                        Console.WriteLine(" Введите обновлённый текст стиха:");
+                        newInfo = Console.ReadLine()!;
+                        poetries[ind].Text = newInfo;
+                        break;
+                    case 5:
+                        Console.WriteLine(" Введите обновлённую тему стиха:");
+                        newInfo = Console.ReadLine()!;
+                        poetries[ind].Theme = newInfo;
+                        break;                   
+                }
             }
+
         }
     }
 }
