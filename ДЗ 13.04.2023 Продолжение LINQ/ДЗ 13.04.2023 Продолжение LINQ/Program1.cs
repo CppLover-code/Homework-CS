67e8961a11169c4d1b8192785a5a93c30489e4a4
Anna Boiko <annaboiko377@gmail.com>
2023-05-08 13:57:38 +0300

    !

--- ДЗ 13.04.2023 Продолжение LINQ/ДЗ 13.04.2023 Продолжение LINQ/Program1.cs
+++ ДЗ 13.04.2023 Продолжение LINQ/ДЗ 13.04.2023 Продолжение LINQ/Program1.cs
@@ -4,13 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+
+
         }
         class Phone
         {
             public string? Title { get; set; }
-            public string? Manufacturer { get; set; } 
-            public double Price { get; set; }   
+            public string? Manufacturer { get; set; }
+            public double Price { get; set; }
             public DateTime Date { get; set; }
 
             public Phone() { }
@@ -21,6 +22,16 @@
                 this.Price = price;
                 this.Date = date;
             }
+            public override string ToString()
+            {
+                return $"Phone model:{Title}\nManufacturer:{Manufacturer}\nPrice:{Price}\n Date of issue:{Date}";
+            }
+        }
+        class Phones
+        {
+            public Phone[] phones;
+            public Phones(Phone[] phones) => this.phones = phones;
 
+        }
     }
 }
