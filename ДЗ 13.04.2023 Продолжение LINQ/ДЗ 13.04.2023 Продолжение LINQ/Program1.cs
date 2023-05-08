629671a2fafdc9e813099af26034c695207dc2b7
Anna Boiko <annaboiko377@gmail.com>
2023-05-08 14:04:55 +0300

    !

--- ДЗ 13.04.2023 Продолжение LINQ/ДЗ 13.04.2023 Продолжение LINQ/Program1.cs
+++ ДЗ 13.04.2023 Продолжение LINQ/ДЗ 13.04.2023 Продолжение LINQ/Program1.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-
+            var phones = new Phones(new Phone[]
+                {
+                    new Phone("Iphone 14", "Apple", 1500, new DateTime(2020, 5, 15)),
+                    new Phone("Iphone XR", "Apple", 579, new DateTime(2018, 3, 10)),
+                    new Phone("H13", "Nokia", 285, new DateTime(2018, 4, 10)),
+                    new Phone("Redmi 10", "Xiaomi", 399, new DateTime(2021, 2, 14)),
+                    new Phone("ThinkPhone 8", "Motorola", 1000, new DateTime(2023, 2, 18)),
+                    new Phone("Samsung A8", "Samsung", 1200, new DateTime(2020,10,25)),
+                    new Phone("Hot 20", "Infinix", 100, new DateTime(2019, 10, 25)),
+                    new Phone("Samsung S10", "Samsung", 300, new DateTime(2019,11,15)),
+                    new Phone("Iphone 10", "Apple", 500, new DateTime(2020, 5, 15)),
+                    new Phone("Samsung S20", "Samsung", 350, new DateTime(2020,8,13)),
+                    new Phone("Nokia 5S", "Nokia", 125, new DateTime(2015,7,25)),
+                    new Phone("Redmi 10", "Xiaomi", 600, new DateTime(2021, 2, 14)),
+                    new Phone("Blade V40", "ZTE", 150, new DateTime(2020, 6, 17)),
+                    new Phone("ThinkPhone 8", "Motorola", 1000, new DateTime(2022, 5, 8))
+                });
 
         }
         class Phone
@@ -31,7 +47,6 @@
         {
             public Phone[] phones;
             public Phones(Phone[] phones) => this.phones = phones;
-
         }
     }
 }
