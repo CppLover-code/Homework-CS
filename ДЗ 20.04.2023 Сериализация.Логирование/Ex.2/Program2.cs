c390c3dcecab6ccbc25065d616344b262a161a4f
Anna Boiko <annaboiko377@gmail.com>
2023-05-15 02:44:37 +0300

    !

---
+++ ДЗ 20.04.2023 Сериализация.Логирование/Ex.2/Program2.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex._2
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ДЗ 20.04.2023 Сериализация.Логирование/ДЗ 20.04.2023 Сериализация.Логирование/Program1.cs
@@ -0,0 +1,10 @@
+﻿namespace ДЗ_20._04._2023_Сериализация.Логирование
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
