c57ef34ef699072930c80ee309938b8bbb8e0fb1
Anna Boiko <annaboiko377@gmail.com>
2023-05-16 01:40:26 +0300

    !

--- ДЗ 20.04.2023 Сериализация.Логирование/Ex.2/Program2.cs
+++ ДЗ 20.04.2023 Сериализация.Логирование/Ex.2/Program2.cs
@@ -17,6 +17,14 @@ namespace Ex._2
         {
            Console.Title = "Генерация фейковых пользователей";
 
+            List<User> FakeUsers = new List<User>()
+            {
+                new User("Sam", "Smith", "+90 (532)-106-89-15", "smith94@gmail.com", "Istanbul"),
+                new User("Briana", "Tart", "+90 (546)-215-14-32", "tart05bri@gmail.com", "Izmir"),
+                new User("Mitch", "Dewitt", "+90 (532)-954-71-44", "dewitt2435@gmail.com", "Sakarya"),
+                new User("Jane", "Ostin", "+90 (546)-887-12-84", "ostinJ78@gmail.com", "Ankara")
+            };
+
             #region NLog Initializator
 
             var config = new NLog.Config.LoggingConfiguration();
@@ -30,33 +38,37 @@ namespace Ex._2
 
             var logfile = new FileTarget();
             config.AddRule(LogLevel.Error, LogLevel.Fatal, logfile);
+
             logfile.CreateDirs = true;
             logfile.FileName = $"logs{Path.DirectorySeparatorChar}lastlog2.log";
 
             logfile.Layout = @"${counter}|[${date:format=yyyy-MM-dd HH\:mm\:ss}] [${logger}/${uppercase: ${level}}] >> ${message} ${exception: format=ToString}";
 
-            logfile.KeepFileOpen = true;
+            logfile.KeepFileOpen = true; // файл для записи постоянно открыт
 
             NLog.LogManager.Configuration = config;
 
             #endregion NLog Initializator
-            //Log.Information("Старт генерации фейковых пользователей");
 
-            for (int i = 0; i < 10; i++)
-            {
-                var fakeUser = new User
+            Console.WriteLine(" Генерация фейковых пользователей!");
+            try
+            {               
+                for (int i = 0; i < 10; i++)
                 {
-                    FirstName = Name.First(),
-                    LastName = Name.Last(),
-                    Phone = Phone.Number(),
-                    Email = Internet.Email(),
-                    Address = Address.FullAddress()
-                };
-
-                Logger.Debug("Сгенерирован фейковый пользователь: {@User}", fakeUser);
+                    Console.WriteLine($" Сгенерирован фейковый пользователь {FakeUsers[i].FirstName} {FakeUsers[i].LastName}");
+                    Logger.Info($"Фейковый пользователь {FakeUsers[i].FirstName} {FakeUsers[i].LastName}");
+                    Logger.Warn($"Фейковый пользователь {FakeUsers[i].FirstName} {FakeUsers[i].LastName}");
+                    Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Error " + ex.Message);
+                Logger.Fatal("Fatal Error! " + ex);
             }
 
-            //Logger.Info("Генерация фейковых пользователей завершена");
+
+            Console.WriteLine(" Генерация фейковых пользователей завершена!");
 
             Console.ReadLine();
         }
@@ -69,5 +81,15 @@ namespace Ex._2
         public string? Phone { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
+
+        public User() { }
+        public User(string fname, string lname, string phone, string email, string address)
+        {
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.Phone = phone;
+            this.Email = email;
+            this.Address = address;
+        }
     }
 }
